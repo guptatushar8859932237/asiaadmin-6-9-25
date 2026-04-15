2dfa9e1bb906038a4d84f6de0e5e45d765ac6fd7
guptatushar8859932237 <mobappssolutions185@gmail.com>
2026-04-15 10:56:48 +0530

    test work admin

--- src/pages/WarehouseDetails.jsx
+++ src/pages/WarehouseDetails.jsx
@@ -5,6 +5,7 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { toast } from "react-toastify";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Swal from "sweetalert2";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import CloseIcon from "@mui/icons-material/Close";
 import { FaEdit } from "react-icons/fa";
 import { Box, Modal, Button, Typography, IconButton } from "@mui/material";
@@ -17,6 +18,7 @@ export default function WarehouseDetails() {
   const navigate = useNavigate();
   const [apidata, setApidata] = useState([]);
   const [isModalOpen3, setIsModalOpen3] = useState(false);
+  const [isModalviewsection3, setIsModalviewsection3] = useState(false);
   const [show1, setShow1] = useState(false);
   const postassiandata = async () => {
     const payloca = {
@@ -144,6 +146,30 @@ export default function WarehouseDetails() {
     }
     setIsModalOpen3(true);
   };
+  const handleEditClickview = async (freight_ID) => {
+    console.log(freight_ID);
+    const payload = {
+      id: freight_ID,
+    };
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}getWarehouseProductById`,
+        payload,
+      );
+      if (response?.data?.success) {
+        setSelectedData(response.data.data);
+      } else {
+        toast.error("Failed to fetch product details");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setIsModalviewsection3(true);
+  };
+  const handleCloseadadModal3 = () => {
+    setIsModalviewsection3(false);
+    setSelectedData({});
+  }
 
   const handlechangepro = (e) => {
     const { name, value } = e.target;
@@ -197,6 +223,21 @@ export default function WarehouseDetails() {
       toast.error("Something went wrong");
     }
   };
+
+  const totalWeight = apidata.reduce(
+  (sum, item) => sum + Number(item.weight || 0),
+  0
+);
+
+const totalPackages = apidata.reduce(
+  (sum, item) => sum + Number(item.packages || 0),
+  0
+);
+
+const totalDimension = apidata.reduce(
+  (sum, item) => sum + Number(item.dimension || 0),
+  0
+);
   return (
     <div className="wpWrapper">
       <div className="container-fluid">
@@ -316,22 +357,22 @@ export default function WarehouseDetails() {
                               </td>
                               <td>
                                 <p className="client_para1">
-                                  {info.costs_to_collect}
+                                  {info.order_warehouse_cost}
                                 </p>
                               </td>
                             </tr>
                             <tr>
-                              <td>
+                              {/* <td>
                                 <p className="client_para1">Warehouse</p>
-                              </td>
+                              </td> */}
                               <td>
                                 <p className="client_para1"></p>
                               </td>
-                              <td>
+                              {/* <td>
                                 <p className="client_para1">
                                   {info.warehouse_cost}
-                                </p>
-                              </td>
+                                </p> */}
+                              {/* </td> */}
                             </tr>
                             <tr>
                               <td>
@@ -342,14 +383,14 @@ export default function WarehouseDetails() {
                               </td>
                               <td>
                                 <p className="client_para1 ">
-                                  {info.costs_to_collect}
+                                  {info.order_costs_to_collect}
                                 </p>
                               </td>
                             </tr>
                             <tr>
                               <td>
                                 <p className="client_para1 ">
-                                  Origin On Carriage Costs
+                                 Handling Costs
                                 </p>
                               </td>
                               <td>
@@ -357,7 +398,7 @@ export default function WarehouseDetails() {
                               </td>
                               <td>
                                 <p className="client_para1 ">
-                                  {info.origin_oncarriage_costs}
+                                  {info.handling_cost}
                                 </p>
                               </td>
                             </tr>
@@ -400,7 +441,7 @@ export default function WarehouseDetails() {
                               </td>
                               <td>
                                 <p className="client_para1">
-                                  {info.total_weight}
+ {totalWeight   }
                                 </p>
                               </td>
                             </tr>
@@ -410,7 +451,7 @@ export default function WarehouseDetails() {
                               </td>
                               <td>
                                 <p className="client_para1">
-                                  {info?.total_cbm}
+                                  {totalDimension}
                                 </p>
                               </td>
                             </tr>
@@ -420,7 +461,7 @@ export default function WarehouseDetails() {
                               </td>
                               <td>
                                 <p className="client_para1">
-                                  {info.total_packages}
+                                  {totalPackages}
                                 </p>
                               </td>
                             </tr>
@@ -473,7 +514,7 @@ export default function WarehouseDetails() {
                   <label>Attach Quotation :</label>
                   {info.attachment_Estimate && (
                     <a
-                      href={`${process.env.REACT_APP_BASE_URL}document/${info?.attachment_Estimate}`}
+                      href={`${process.env.REACT_APP_BASE_URLdocument}${info?.attachment_Estimate}`}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="view_docu ms-2"
@@ -520,6 +561,14 @@ export default function WarehouseDetails() {
                           <td>{item.dimension}</td>
                           <td>{item.weight}</td>
                           <td>
+                             {item?.id ? (
+                              <VisibilityIcon
+                                onClick={() => handleEditClickview(item.id)}
+                                style={{ color: "#1d2044", cursor: "pointer" }}
+                              />
+                            ) : (
+                              ""
+                            )}
                             {item?.id ? (
                               <FaEdit
                                 onClick={() => handleEditClick(item.id)}
@@ -536,6 +585,7 @@ export default function WarehouseDetails() {
                             ) : (
                               ""
                             )}
+                           
                           </td>
                         </tr>
                       </>
@@ -790,6 +840,265 @@ export default function WarehouseDetails() {
                 Edit Product
               </Button>
             </Box>
+          </Box>
+        </Modal>
+        <Modal open={isModalviewsection3} onClose={handleCloseadadModal3}>
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "80%",
+              bgcolor: "background.paper",
+              boxShadow: 24,
+              p: 4,
+              borderRadius: 2,
+              maxHeight: "90vh",
+              overflowY: "auto",
+            }}
+          >
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              mb={2}
+            >
+              <Typography variant="h6">Warehouse Detail</Typography>
+              <IconButton onClick={handleCloseModal3}>
+                <CloseIcon />
+              </IconButton>
+            </Box>
+            <div className="newModalGap  noFormaControl">
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label className="form-label">Product Description</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="product description"
+                    disabled
+                    value={selectedData.product_description || ""}
+                    onChange={handlechangepro}
+                    name="product_description"
+                  />
+                </div>
+                <div className="col-md-6 noFormaControl">
+                  <label className="form-label">Harzadous</label>
+                  <select
+                    onChange={handlechangepro}
+                    value={selectedData.Hazardous || ""}
+                    name="Hazardous"
+                    disabled
+                  >
+                    <option>Select...</option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                  </select>
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label className="form-label">Warehouse Ref.</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={selectedData.warehouse_ref || ""}
+                    placeholder="warehouse reference"
+                    disabled
+                    name="warehouse_ref"
+                    onChange={handlechangepro}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label className="form-label">Data Received</label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    name="date_received"
+                    disabled
+                    value={
+                      selectedData.date_received
+                        ? new Date(selectedData.date_received)
+                            .toISOString()
+                            .split("T")[0]
+                        : ""
+                    }
+                    onChange={handlechangepro}
+                  />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-6 noFormaControl">
+                  <label className="form-label">Package Type</label>
+                  <select
+                    name="package_type"
+                    disabled
+                    value={selectedData.package_type || ""}
+                    onChange={handlechangepro}
+                  >
+                    <option value="">Select...</option>
+                    <option value="box">Box</option>
+                    <option value="crate">Crate</option>
+                    <option value="pallet">Pallet</option>
+                    <option value="bags">Bags</option>
+                  </select>
+                </div>
+                <div className="col-md-6">
+                  <label className="form-label">Total Packages</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    disabled
+                    placeholder="0.00"
+                    onKeyPress={handlekey}
+                    value={selectedData.packages || ""}
+                    name="packages"
+                    onChange={handlechangepro}
+                  />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label className="form-label">Dimension</label>
+                  <input
+                    type="text"
+                    name="dimension"
+                    value={selectedData.dimension || ""}
+                    className="form-control"
+                    placeholder="0.00"
+                    disabled
+                    onChange={handlechangepro}
+                    onKeyPress={handlekey}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label className="form-label">Weight</label>
+                  <input
+                    type="text"
+                    disabled
+                    name="weight"
+                    className="form-control"
+                    value={selectedData.weight || ""}
+                    placeholder="0.00"
+                    onKeyPress={handlekey}
+                    onChange={handlechangepro}
+                  />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label className="form-label">Supplier Address</label>
+                  <input
+                    type="text"
+                    name="supplier_address"
+                    disabled
+                    value={selectedData.supplier_address || ""}
+                    className="form-control"
+                    placeholder="0.00"
+                    onChange={handlechangepro}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label className="form-label">Supplier</label>
+                  <input
+                    type="text"
+                    name="supplier"
+                    value={selectedData.supplier || ""}
+                    className="form-control"
+                    placeholder="0.00"
+                    disabled
+                    onChange={handlechangepro}
+                  />
+                </div>
+              </div>
+
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label className="form-label">Warehouse Order</label>
+                  <input
+                    type="text"
+                    name="warehouse_order_id"
+                    className="form-control"
+                    placeholder="0.00"
+                    value={selectedData.warehouse_order_id || ""}
+                    onChange={handlechangepro}
+disabled
+                    onKeyPress={handlekey}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label className="form-label">Costs to Collect</label>
+                  <input
+                    type="text"
+                    name="costs_to_collect"
+                    className="form-control"
+                    placeholder="0.00"
+                    disabled
+                    value={selectedData.costs_to_collect || ""}
+                    onKeyPress={handlekey}
+                    onChange={handlechangepro}
+                  />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label className="form-label">Warehouse dispatch</label>
+                  <input
+                    type="text"
+                    value={selectedData.warehouse_dispatch || ""}
+                    name="warehouse_dispatch"
+                    className="form-control"
+                    placeholder="0.00"
+                    disabled
+                    onChange={handlechangepro}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label className="form-label">Waybill Ref</label>
+                  <input
+                    type="text"
+                    value={selectedData.waybill_ref || ""}
+                    name="waybill_ref"
+                    className="form-control"
+                    placeholder="0.00"
+                    disabled
+                    onChange={handlechangepro}
+                  />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label className="form-label">Warehouse Cost</label>
+                  <input
+                    type="text"
+                    name="warehouse_cost"
+                    className="form-control"
+                    placeholder="0.00"
+                    disabled
+                    value={selectedData.warehouse_cost || ""}
+                    onChange={handlechangepro}
+                    onKeyPress={handlekey}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label className="form-label">Cost to Dispatch</label>
+                  <input
+                    type="text"
+                    name="cost_to_dispatch"
+                    value={selectedData.cost_to_dispatch || ""}
+                    className="form-control"
+                    disabled
+                    placeholder="0.00"
+                    onKeyPress={handlekey}
+                    onChange={handlechangepro}
+                  />
+                </div>
+              </div>
+              <div className="row mb-3"></div>
+              <div class="modal-footer"></div>
+            </div>
+           
           </Box>
         </Modal>
       </div>

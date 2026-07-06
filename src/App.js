87e38cd5b750458de8d9dd04d26180d10d8035fd
nehavishvas <104732802+nehavishvas@users.noreply.github.com>
2026-07-06 10:01:47 +0530

    Invoice, Quote section -changes

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ const Uniovwersalpage = lazy(() => import("./components/Uniovwersalpage"));
 export default function App() {
   const [text, setText] = useState("");
   const [permission, setPermission] = useState("");
-  console.log("02-07-26", "17:39");
+  console.log("06-07-26", "10:01");
   return (
     <MyContext1.Provider value={{ text, setText }}>
       <MyContext2.Provider value={{ permission, setPermission }}>
